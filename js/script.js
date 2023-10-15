453366e50509e6aa39a7ec33456f4e54d5f03e84
Nicolas Henrique <nicolashenrisilva@outlook.com>
2023-10-15 03:41:12 -0300

    resposivo home pronto

--- js/script.js
+++ js/script.js
@@ -1,10 +1,17 @@
 window.onscroll = function () {
     let top = window.scrollY;
 
-    if (top > 550) {
-        document.getElementById("topo2").classList.add("menufixo");
+    if (top > 120) {
+        document.getElementById("menuR").classList.add("menufixo");
     }else{
-        document.getElementById("topo2").classList.remove("menufixo");
+        document.getElementById("menuR").classList.remove("menufixo");
     }
     
+}
+
+document.querySelector(".abrirMenu").onclick = function () {
+    document.documentElement.classList.add("menuMobile");
+}
+document.querySelector(".fecharMenu").onclick = function () {
+    document.documentElement.classList.remove("menuMobile");
 }
